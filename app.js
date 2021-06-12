f080d6c7408090634caf1ee5ffe08c2239b47a5f
Javier Conde <javo.edu83@gmail.com>
2021-06-12 12:39:48 -0500

    Pruebas

---
+++ app.js
@@ -0,0 +1,32 @@
+//1
+/*const nombre = "Sebastián Yabiku";
+const edad = 33;
+
+const nombre = prompt("Ingresa tu nombre");
+const edad = prompt("Digita tu edad");
+
+document.write("Hola mi nombre es " + nombre + " y mi edad es "+ edad);*/
+
+
+//2
+/*const numero1 = prompt("Digita el primer número");
+const numero2 = prompt("Digita el segundo número");
+const numero3 = prompt("Digita el tercer número");
+
+let resultado1;
+resultado1 = Math.pow(numero1, 3);
+
+let resultado2;
+resultado2 = Math.pow(numero2, 3);
+
+let resultado3;
+resultado3 = Math.pow(numero3, 3);
+
+let resultado4;
+resultado4 = resultado1 + resultado2 + resultado3;
+
+
+document.write("La suma de sus cubos es "+ resultado4);
+*/
+
+//3
